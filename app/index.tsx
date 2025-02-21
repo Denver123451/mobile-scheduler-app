a0c949c4c53377017c0de33335bf35691c1341d0
Yura Zhekov <yuriy.j@ezlogz.com>
2025-02-21 10:47:04 +0200

    Form

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,55 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+import Colors from '@/constants/colors';
+import { useState } from 'react';
+import { calendarTheme } from '@/constants/calendarTheme';
 
 export default function HomeScreen() {
+  const [selectedDate, setSelectedDate] = useState('');
+  const [eventName, setEventName] = useState('');
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
+  const [repeat, setRepeat] = useState('weekly');
+
+  const handleDayPress = (day: any) => {
+    setSelectedDate(day.dateString);
+    console.log('Выбрана дата:', selectedDate);
+  };
   return (
     <>
       <View style={styles.titleContainer}>
-        <Text>Hello World!</Text>
+        <Text style={styles.title}>Event Scheduling App</Text>
+        <Calendar
+          style={styles.calendar}
+          onDayPress={handleDayPress}
+          markedDates={{
+            [selectedDate]: {
+              selected: true,
+              selectedColor: Colors.yellow500,
+            },
+          }}
+          theme={calendarTheme}
+        />
+
+        <View style={styles.form}>
+          <Text style={styles.label}>Event Name</Text>
+          <TextInput
+            style={styles.input}
+            value={eventName}
+            onChangeText={setEventName}
+            placeholder="Every Name"
+          />
+
+          <View>
+            <Text style={styles.label}>Starts</Text>
+            <TextInput
+              style={styles.input}
+              value={startTime}
+              onChangeText={setStartTime}
+              placeholder="Jan 28, 2025 03:00 PM"
+            />
+          </View>
+        </View>
       </View>
     </>
   );
@@ -15,5 +60,30 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    paddingBottom: 24,
+    color: Colors.yellow500,
+    fontSize: 24,
+  },
+  calendar: {
+    borderRadius: 10,
+  },
+  form: {
+    marginTop: 16,
+    paddingHorizontal: 16,
+  },
+  label: {
+    marginTop: 12,
+    marginBottom: 4,
+    fontWeight: '600',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: Colors.grey500,
+    borderRadius: 8,
+    paddingVertical: 8,
+    fontSize: 16,
   },
 });
---
+++ constants/calendarTheme.ts
@@ -0,0 +1,18 @@
+import Colors from '@/constants/colors';
+
+export const calendarTheme = {
+  backgroundColor: Colors.white,
+  calendarBackground: Colors.white,
+  textSectionTitleColor: Colors.grey500,
+  selectedDayBackgroundColor: Colors.yellow500,
+  selectedDayTextColor: Colors.white,
+  todayTextColor: Colors.yellow500,
+  dayTextColor: Colors.gray800,
+  textDisabledColor: Colors.gray300,
+  arrowColor: Colors.yellow500,
+  monthTextColor: Colors.gray800,
+  indicatorColor: Colors.gray800,
+  textDayFontSize: 16,
+  textMonthFontSize: 18,
+  textDayHeaderFontSize: 14,
+};
